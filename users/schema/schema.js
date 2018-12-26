cc4b119e6249b3eb79c5665b6eb1eebb949b85eb
hemant <nhemnt@gmail.com>
2018-12-27 00:52:14 +0530

    integrating schema and rootQuery

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,10 +1,18 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
-} = graphQL;
+    GraphQLInt,
+    GraphQLSchema
+} = graphql;
 
+const users = [
+    { id: '231', firstName: 'Hemant', age: 22 },
+    { id: '233', firstName: 'Hitesh', age: 21 },
+    { id: '234', firstName: 'Hunny', age: 20 },
+    { id: '235', firstName: 'Himanshu', age: 24 },
+]
 const UserType = new GraphQLObjectType({
     name: 'Users',
     fields: {
@@ -12,4 +20,21 @@ const UserType = new GraphQLObjectType({
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
     }
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        Users: {
+            type: 'UserType',
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) { 
+                return _.find(users, { id: args.id })
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
